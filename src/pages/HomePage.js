07ab4fa56ea1bbfb8b211090f28c30513f53bfc9
KarthikeyankarJ <karthikeyanjk@outlook.com>
2023-11-26 12:31:51 +0530

    changed

---
+++ src/pages/HomePage.js
@@ -0,0 +1,13 @@
+
+import Loyout from '../components/layout/Loyout'
+const HomePage = () => {
+  return (
+    <Loyout>
+      <h1>Home Page</h1>
+    </Loyout>
+
+
+  )
+}
+
+export default HomePage
